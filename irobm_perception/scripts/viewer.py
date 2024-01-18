87d357bdec1bb4b1803483b45fbcf3503d7e9c4a
Haris Čavkić <dr.cavkic.developer@gmail.com>
2024-01-18 17:00:12 +0100

    Unclean working version of combining PCs, segmenting and doing icp to find the position and orientation

--- irobm_perception/scripts/viewer.py
+++ irobm_perception/scripts/viewer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.8
 from pathlib import Path
 import copy
+import random
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -12,7 +13,8 @@ import open3d as o3d
 # from utils import visualize, combine_pointclouds
 
 
-DATA_PATH = Path(__file__).parent.parent / "data/backup"
+# DATA_PATH = Path(__file__).parent.parent / "data/backup"
+DATA_PATH = Path(__file__).parent.parent / "data"
 
 
 # NO ROS VERSION
@@ -67,28 +69,34 @@ def combine_pointclouds(pcds,
     return pcd_combined_down
 
 
-def draw_registration_results(pc1, pc2=None, transformation=None):
-    if not isinstance(pc1, list):
-        assert pc2 is not None
-        pc1_temp = copy.deepcopy(pc1)
-        pc2_temp = copy.deepcopy(pc2)
-        pc1_temp.paint_uniform_color([1, 0.706, 0])
-        pc2_temp.paint_uniform_color([0, 0.651, 0.929])
-        # source_temp.transform(transformation)
-        o3d.visualization.draw_geometries([pc1_temp, pc2_temp])
-    else:
-        point_clouds = pc1
-        colors = [[1, 0.706, 0], [0, 0.651, 0.929]]
-        # Create a list of deepcopy of point clouds
-        point_clouds_temp = [copy.deepcopy(pc) for pc in point_clouds]
+def draw_registration_results(source, target, transformation=None):
+    source_temp = copy.deepcopy(source)
+    target_temp = copy.deepcopy(target)
+    source_temp.paint_uniform_color([1, 0.706, 0])
+    target_temp.paint_uniform_color([0, 0.651, 0.929])
+    source_temp.transform(transformation)
+    o3d.visualization.draw_geometries([source_temp, target_temp],
+                                      zoom=0.4559,
+                                      front=[0.6452, -0.3036, -0.7011],
+                                      lookat=[1.9892, 2.0208, 1.8945],
+                                      up=[-0.2779, -0.9482, 0.1556])
 
-        # Set random colors for visualization
-        for i, pc_temp in enumerate(point_clouds_temp):
-            color = np.random.rand(3, ) if len(colors) == 0 else colors.pop()
-            pc_temp.paint_uniform_color(color)
 
-        # Visualize the point clouds
-        o3d.visualization.draw_geometries(point_clouds_temp)
+def preprocess_point_cloud(pcd, voxel_size):
+    print(":: Downsample with a voxel size %.3f." % voxel_size)
+    pcd_down = pcd.voxel_down_sample(voxel_size)
+
+    radius_normal = voxel_size * 2
+    print(":: Estimate normal with search radius %.3f." % radius_normal)
+    pcd_down.estimate_normals(
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
+
+    radius_feature = voxel_size * 5
+    print(":: Compute FPFH feature with search radius %.3f." % radius_feature)
+    pcd_fpfh = o3d.pipelines.registration.compute_fpfh_feature(
+        pcd_down,
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
+    return pcd_down, pcd_fpfh
 
 
 def load_and_view():
@@ -102,16 +110,32 @@ def load_and_view():
     point_cloud2.points = o3d.utility.Vector3dVector(pc2)
     point_cloud3 = o3d.geometry.PointCloud()
     point_cloud3.points = o3d.utility.Vector3dVector(pc3)
-    # draw_registration_results([point_cloud1, point_cloud2, point_cloud3])
+    # o3d.visualization.draw_geometries([point_cloud1, point_cloud2, point_cloud3])
     # combine_pointclouds([point_cloud1, point_cloud2])
-    combined_pcd = combine_pointclouds([point_cloud1, point_cloud2, point_cloud3])
+    combined_pcd = combine_pointclouds([point_cloud1, point_cloud2, point_cloud3], visualise=False)
     assert isinstance(combined_pcd, o3d.geometry.PointCloud)
-    # o3d.visualization.draw_geometries([combined_pcd])
+    #o3d.visualization.draw_geometries([combined_pcd])
 
+    ###
+    # recorded pc has outliers this is brute force to remove these
+    points_array = np.array(list(combined_pcd.points))
+    # todo check weather this all is necessary
+    mask = np.where(points_array[:, 2] < 1.3, True, False)
+    mask3 = np.where(points_array[:, 1] < 0.8, True, False)
+    mask2 = np.where(points_array[:, 0] < 1.3, True, False)
+    mask = np.logical_and(mask, np.logical_and(mask2, mask3))
+    points_array = points_array[mask]
+    points_pcd = o3d.geometry.PointCloud()
+    points_pcd.points = o3d.utility.Vector3dVector(points_array)
+    combined_pcd.paint_uniform_color([1, 0, 0])
+    points_pcd.paint_uniform_color([0, 1, 0])
+    # o3d.visualization.draw_geometries([combined_pcd, points_pcd])
+    #####
 
     # filtering
     # todo test this part
     # probably not needed
+    combined_pcd = points_pcd
     filtered_pcd = combined_pcd.remove_statistical_outlier(nb_neighbors=16, std_ratio=10)
     outliers = combined_pcd.select_by_index(filtered_pcd[1], invert=True)
     outliers.paint_uniform_color([1, 0, 0])
@@ -120,6 +144,7 @@ def load_and_view():
     # end filtering
 
     # normals
+    combined_pcd = filtered_pcd
     nn_distance = np.mean(combined_pcd.compute_nearest_neighbor_distance())
     radius_normals = 4 * nn_distance
     combined_pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normals, max_nn=16),
@@ -138,21 +163,105 @@ def load_and_view():
 
     inlier_cloud.paint_uniform_color([1., 0., 0.])
     cubes_cloud.paint_uniform_color([0.6, .6, .6])
-    o3d.visualization.draw_geometries([cubes_cloud])
+    #o3d.visualization.draw_geometries([cubes_cloud])
 
     # end of RANSAC
 
-    # generate cube model
+    # dbscan for rest
+    # works only if cubes not near each other (really near)
     assert isinstance(cubes_cloud, o3d.geometry.PointCloud)
+    labels = np.array(cubes_cloud.cluster_dbscan(eps=0.005, min_points=5))
+    max_labels = labels.max()
+
+    colors = plt.get_cmap("tab10")(labels / max_labels if max_labels > 0 else 1)
+    colors[labels < 0] = 0
+    cubes_cloud.colors = o3d.utility.Vector3dVector(colors[:, :3])
+
+    # o3d.visualization.draw_geometries([cubes_cloud])
+
+    segmented_cubes = list()
+
+    for i in range(0, max_labels + 1):
+        print("LABEL: ", i)
+        indices_to_extract = np.where(labels == i)
+
+        # Convert PointCloud to a NumPy array
+        cubes_points_ndarray = np.asarray(cubes_cloud.points)
+
+        # Extract points based on indices
+        segment = cubes_points_ndarray[indices_to_extract]
+
+        # Create a new PointCloud from the selected points
+        selected_cloud = o3d.geometry.PointCloud()
+        selected_cloud.points = o3d.utility.Vector3dVector(segment)
+        segmented_cubes.append(selected_cloud)
+        #o3d.visualization.draw_geometries([selected_cloud])
+
+    # Global alignment
+    all_transformations = list()
+    cubes_list = list()
+    for segment in segmented_cubes:
+        cube_model = create_cube_model(0.046)
+        cube_model_cloud = mesh_to_pcd(cube_model, v2=False, hollow_cube=True, size=0.046)
+        # set cube to 0 position
+        cube_model_cloud.translate(-cube_model_cloud.get_center(), relative = False)
+
+        initial_transformation = np.eye(4)
+        initial_transformation[:3, 3] = get_icp_start(segment) #push near target cloud
+        cube_model_cloud.transform(initial_transformation)
+
+        threshold = 10
+        result = do_icp(cube_model_cloud, segment, distance_threshold=threshold)
+        transformation, fitness = result
+        ##test visual
+        cube_model_cloud.transform(transformation)
+        cube_model_cloud.paint_uniform_color([1., 0., 0.])
+        segment.paint_uniform_color([0.6, .6, .6])
+        cubes_list.append(cube_model_cloud)
+        #o3d.visualization.draw_geometries([segment, cube_model_cloud])
+        #o3d.visualization.draw_geometries([cubes_cloud, cube_model_cloud])
+
+        all_transformations.append(np.dot(transformation, initial_transformation))
+
+        """cube_model_new = create_cube_model(0.046)
+        cube_model_cloud_new = mesh_to_pcd(cube_model_new, v2=False, hollow_cube=True, size=0.046)
+        cube_model_cloud_new.translate(-cube_model_cloud_new.get_center(), relative=False)
+        cube_model_cloud_new.transform(final_trafo)
+        o3d.visualization.draw_geometries([segment, cube_model_cloud_new])"""
+
+
+
+        """for iter_nr in range(2):
+            threshold = 10 if iter_nr == 0 else 0.1
+            result = do_icp(cube_model_cloud, segment, distance_threshold=threshold)
+            transformation, fitness = result
+            ##test visual
+            cube_model_cloud.transform(transformation)
+            cube_model_cloud.paint_uniform_color([1., 0., 0.])
+            segment.paint_uniform_color([0.6, .6, .6])
+            o3d.visualization.draw_geometries([segment, cube_model_cloud])
+            o3d.visualization.draw_geometries([cubes_cloud, cube_model_cloud])
+
+        voxel_size = 0.0005  # test
+        source_down, source_fpfh = preprocess_point_cloud(cube_model_cloud, voxel_size)
+        target_down, target_fpfh = preprocess_point_cloud(segment, voxel_size)
+        result_ransac = execute_global_registration(source_down, target_down,
+                                                    source_fpfh, target_fpfh,
+                                                    voxel_size)
+       """
+    all_clouds = [cubes_cloud]
+    all_clouds.extend(cubes_list)
+    o3d.visualization.draw_geometries(all_clouds)
+    exit()
+    # generate cube model
+
     cube_model = create_cube_model(0.05)
     cube_model_cloud = mesh_to_pcd(cube_model)
-    #cube_model_cloud.translate(cubes_cloud.get_center())
+    # cube_model_cloud.translate(cubes_cloud.get_center())
     result = multi_start_icp(cube_model_cloud, cubes_cloud)
 
     # cube_model_cloud.paint_uniform_color([1., 0., 0.])
     # o3d.visualization.draw_geometries([cube_model_cloud, cubes_cloud])
-
-    exit()
 
     labels = np.array(cubes_cloud.cluster_dbscan(eps=.005, min_points=40))
     max_labels = labels.max()
@@ -161,13 +270,29 @@ def load_and_view():
         to_visualise = cubes_cloud.select_by_index(np.where(labels == i)[0])
         o3d.visualization.draw_geometries([to_visualise])
 
-    exit()
-
     colors = plt.get_cmap("tab10")(labels / (max_labels if max_labels > 0 else 1))
     colors[labels < 0] = 0
     cubes_cloud.colors = o3d.utility.Vector3dVector(colors[:, :3])
     o3d.visualization.draw_geometries([cubes_cloud])
     print("")
+
+
+def execute_global_registration(source_down, target_down, source_fpfh,
+                                target_fpfh, voxel_size):
+    distance_threshold = voxel_size * 1.5
+    print(":: RANSAC registration on downsampled point clouds.")
+    print("   Since the downsampling voxel size is %.3f," % voxel_size)
+    print("   we use a liberal distance threshold %.3f." % distance_threshold)
+    result = o3d.pipelines.registration.registration_ransac_based_on_feature_matching(
+        source_down, target_down, source_fpfh, target_fpfh, distance_threshold,
+        o3d.pipelines.registration.TransformationEstimationPointToPoint(False),
+        4, [
+            o3d.pipelines.registration.CorrespondenceCheckerBasedOnEdgeLength(
+                0.05),
+            o3d.pipelines.registration.CorrespondenceCheckerBasedOnDistance(
+                distance_threshold)
+        ], o3d.pipelines.registration.RANSACConvergenceCriteria(4000000, 500))
+    return result
 
 
 def get_icp_start(cubes_cloud):
@@ -177,6 +302,34 @@ def get_icp_start(cubes_cloud):
     """
     start = cubes_cloud.get_center()  # just center of the point cloud
     return start
+
+
+def do_icp(cube_model_cloud, segmented_cloud, distance_threshold=10):
+    # initial_pose = np.eye(4)
+    # initial_pose[:3, :3] = np.random.uniform(-0.5, 0.5, size=(3, 3))
+    # initial_pose[:3, 3] = get_icp_start(segmented_cloud)
+
+    cube_model_copy = copy.deepcopy(cube_model_cloud)
+    # cube_model_copy.transform(initial_pose)
+
+    # Apply ICP
+    icp_result = o3d.pipelines.registration.registration_icp(
+        cube_model_copy,  # source
+        segmented_cloud,  # target
+        distance_threshold,
+        np.eye(4),
+        o3d.pipelines.registration.TransformationEstimationPointToPoint()
+    )
+
+    if icp_result.fitness < 0.5:
+        # if normal icp did not work do multi start
+        print("Didnt work. Doing multi start icp")
+        raise NotImplementedError
+        result = multi_start_icp(cube_model_cloud, segmented_cloud)[0]
+    else:
+        result = (icp_result.transformation, icp_result.fitness)
+
+    return result
 
 
 def multi_start_icp(cube_model_cloud, cubes_cloud, num_attempts=100, distance_threshold=0.05):
@@ -201,7 +354,7 @@ def multi_start_icp(cube_model_cloud, cubes_cloud, num_attempts=100, distance_th
         initial_pose = np.eye(4)
         initial_pose[:3, :3] = np.random.uniform(-0.5, 0.5, size=(3, 3))  # Random rotation
         if not used_custom_sample:
-            initial_pose[:3, 3] = get_icp_start(cubes_cloud) # Random translation
+            initial_pose[:3, 3] = get_icp_start(cubes_cloud)  # Random translation
             used_custom_sample = True
         else:
             initial_pose[:3, 3] = np.random.uniform(-0.5, 0.5, size=3)  # Random translation
@@ -222,11 +375,8 @@ def multi_start_icp(cube_model_cloud, cubes_cloud, num_attempts=100, distance_th
         # Store the result if the alignment is good
         if icp_result.fitness > 0.5:  # You can adjust this threshold based on your requirements
             # Find the corresponding points in cubes_cloud
-            corresponding_points = find_corresponding_points(icp_result.transformation, cube_model_cloud, cubes_cloud,
-                                                             distance_threshold)
-            best_alignments.append((icp_result.transformation, icp_result.fitness, corresponding_points))
-            #used_custom_sample = False
-
+            best_alignments.append((icp_result.transformation, icp_result.fitness))
+            # used_custom_sample = False
 
     # Sort the results based on fitness score (higher is better)
     best_alignments.sort(key=lambda x: x[1], reverse=True)
@@ -267,32 +417,61 @@ def find_corresponding_points(transformation, source_cloud, target_cloud, distan
     return corresponding_cloud
 
 
-def mesh_to_pcd(mesh, v2=False):
+def mesh_to_pcd(mesh, v2=False, hollow_cube=True, size=0.05, randomness_factor = 0.001):
     # todo test v1 agains v2 (v2=False vs v2 = True)
-    if v2:
-        nr_points = 9000  # todo test lower number
-        return mesh.sample_points_uniformly(number_of_points=nr_points)
 
     # Define the dimensions and spacing for your point grid
-    width = .05  # Width of the cube
-    height = .05  # Height of the cube
-    depth = .05  # Depth of the cube
+    width = size  # Width of the cube
+    height = size  # Height of the cube
+    depth = size  # Depth of the cube
     spacing = 0.0025  # Spacing between points todo: test bigger value (bigger value => less points)
 
     # Calculate the number of points in each dimension
     num_points_x = int(width / spacing)
     num_points_y = int(height / spacing)
     num_points_z = int(depth / spacing)
+    get_randomness = lambda: 0
+
+    if v2:
+        # Maximum random offset for each point
+        nr_points = 6000  # todo test lower number
+        get_randomness = lambda: random.uniform(-randomness_factor, randomness_factor)
+        return mesh.sample_points_uniformly(number_of_points=nr_points)
 
     # Create a grid of points
     points = []
-    for i in range(num_points_x):
+    if not hollow_cube:
+
+        for i in range(num_points_x):
+            for j in range(num_points_y):
+                for k in range(num_points_z):
+                    x = i * spacing - width / 2
+                    y = j * spacing - height / 2
+                    z = k * spacing - depth / 2
+                    points.append([x, y, z])
+
+    else:
+        for i in range(num_points_x):
+            for j in range(num_points_y):
+                # Add points on the top and bottom faces
+                # points.append([i * spacing - width / 2 + get_randomness(), j * spacing - height / 2 + get_randomness(), -depth / 2 + get_randomness()]) #comment this line for not generating the bottom side
+                points.append([i * spacing - width / 2 + get_randomness(), j * spacing - height / 2 + get_randomness(),
+                               depth / 2+ get_randomness()] )
+
+        for i in range(num_points_x):
+            for k in range(num_points_z):
+                # Add points on the front and back faces
+                # points.append([i * spacing - width / 2+ get_randomness(), -height / 2+ get_randomness(), k * spacing - depth / 2+ get_randomness()])#comment this line for not generating the back side
+                points.append([i * spacing - width / 2 + get_randomness(), height / 2 + get_randomness(),
+                               k * spacing - depth / 2 + get_randomness()])
+
         for j in range(num_points_y):
             for k in range(num_points_z):
-                x = i * spacing - width / 2
-                y = j * spacing - height / 2
-                z = k * spacing - depth / 2
-                points.append([x, y, z])
+                # Add points on the left and right faces
+                points.append([-width / 2 + get_randomness(), j * spacing - height / 2 + get_randomness(),
+                               k * spacing - depth / 2 + get_randomness()])
+                #points.append([width / 2 + get_randomness(), j * spacing - height / 2 + get_randomness(),
+                #               k * spacing - depth / 2 + get_randomness()])
 
     # Convert the list of points to a NumPy array
     points_np = np.array(points, dtype=np.float32)
@@ -318,6 +497,7 @@ def create_cube_model(side_length):
                                                      height=side_length,
                                                      depth=side_length)
 
+    #cube_mesh.remove_vertices_by_index([1])
     # Translate the cube to center it at the origin
     cube_mesh.translate(-np.array(cube_mesh.get_center()))
 
@@ -325,11 +505,12 @@ def create_cube_model(side_length):
 
 
 if __name__ == '__main__':
-    try:
+    load_and_view()
+    """try:
         load_and_view()
     except Exception as exc:
         print("Something went wront")
-        print(exc)
+        print(exc)"""
 
     """
     ROS VERSION
